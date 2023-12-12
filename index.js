769ff93b8dba8ecd7b9c4d7956c3e94682207e87
Azzizah01 <azzizahnur01@gmail.com>
2023-12-12 08:09:38 +0700

    cors

--- index.js
+++ index.js
@@ -1,53 +1,31 @@
 const express = require("express");
-const cors = require("cors");
 const unprotectedRoute = require("./routes/unprotected.route");
-// const { testDatabaseConnection } = require("./utils/testDbConnection");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
 
 const app = express();
 
-//CORS policy for localhost:5173 (React app) to access this server (Express app)
-app.use(
-  cors({})
-);
-
-//Parsing body to JSON
 app.use(express.json());
-
-const { createProxyMiddleware } = require("http-proxy-middleware");
+app.use(cors());
 app.use(
   "/external-server",
   createProxyMiddleware({
-    target:
-      "https://kind-jade-jay-gown.cyclic.apphttps://kind-jade-jay-gown.cyclic.app/product",
+    target: "https://kind-jade-jay-gown.cyclic.app",
     changeOrigin: true,
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json')
-  next()
-})
-
-//Testing routes
 app.get("/test", (req, res) => {
-  console.log(req.body)
   res.send("Test route");
 });
 
-//Routes for unprotected routes
 app.use("/", unprotectedRoute);
 
-
-
-//hello world
 app.get("/", (req, res) => {
-  res.send("Hello World! Hello Eperibodi");
+  res.send("Hello World! Hello guyss");
 });
 
-//Start the server
 app.listen(3000, () => {
-  console.log("Tevo24 server listening on port: ",3000);
+  console.log("Tevou server listening on port: ", 3000);
 });
-
-//Test database connection
-// testDatabaseConnection();
+punyaku;
